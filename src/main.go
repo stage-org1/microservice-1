90c1a50bb07f5b276ea4a0c401d2df0f7b82156d
beppe <beppe.vanrolleghem@gmail.com>
2020-05-16 12:43:07 +0200

    linter error fixed

--- src/main.go
+++ src/main.go
@@ -4,12 +4,16 @@ import (
 	"fmt"
 	"math/rand"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/greet", greet)
 	http.HandleFunc("/unstableGreet", unstableGreet)
-	http.ListenAndServe(":80", nil)
+	err:= http.ListenAndServe(":80", nil)
+	if err != nil {
+		os.Exit(1)
+	}
 }
 
 func greet(w http.ResponseWriter, r *http.Request) {
