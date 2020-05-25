c6263c546f9546787712c47fb2109d184399fae1
beppe <beppe.vanrolleghem@gmail.com>
2020-05-25 18:57:26 +0200

    hi, am i on the air?

--- src/main.go
+++ src/main.go
@@ -18,11 +18,12 @@ func doRequest(w http.ResponseWriter, r *http.Request) {
 	subset := r.Header.Get("subset")
 	w.Header().Set("subset", subset)
 	resp, err := http.Get("http://microservice-2-service")
+	statuscode := resp.StatusCode
 	if (err == nil && resp.Body != nil) {
 		defer resp.Body.Close()
 		body, err := ioutil.ReadAll(resp.Body)
 		if (err == nil) {
-			fmt.Fprintf(w, "request was a success, "+string(body))
+			fmt.Fprintf(w, "request was a success, statuscode: " + string(statuscode) + ", body: " + string(body))
 		} else {
 			http.Error(w, "an error occured:" + err.Error(), http.StatusInternalServerError)
 		}
