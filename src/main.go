c7c752bd133297cb0cd1c7e1d2fc0ecc79e547ec
beppe <beppe.vanrolleghem@gmail.com>
2020-05-25 19:02:29 +0200

    string convert verkeerd gedaan

--- src/main.go
+++ src/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 )
 
 func main() {
@@ -23,7 +24,8 @@ func doRequest(w http.ResponseWriter, r *http.Request) {
 		defer resp.Body.Close()
 		body, err := ioutil.ReadAll(resp.Body)
 		if (err == nil) {
-			fmt.Fprintf(w, "request was a success, statuscode: " + string(statuscode) + ", body: " + string(body))
+
+			fmt.Fprintf(w, "request was a success, statuscode: " + strconv.Itoa(statuscode) + ", body: " + string(body))
 		} else {
 			http.Error(w, "an error occured:" + err.Error(), http.StatusInternalServerError)
 		}
